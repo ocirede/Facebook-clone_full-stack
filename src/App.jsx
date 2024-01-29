606f76b088cde208036cdbcf1be9a10e812c54dd
Federico <federico.diaferia@gmail.com>
2024-01-29 22:18:29 +0100

    user added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import Header from './components/Header';
-import Feed from './components/Feed';
-import Navigation from './components/Navigation';
+import React from "react";
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import Navigation from "./components/Navigation";
+import { useUserContext } from "./context/User-context";
+import SignIn from "./pages/Sign-in";
 function App() {
+  const { user } = useUserContext();
+
   return (
-    <div >
-      
-      <Feed />
+    <div>
+      {user ? (
+        <>
+          <Navigation />
+          <Feed />
+        </>
+      ) : (
+        <SignIn />
+      )}
     </div>
   );
-
 }
 
 export default App;
