2fb38f211e64e5a301c52019303de544261e563e
Federico <federico.diaferia@gmail.com>
2024-01-29 14:03:21 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import SignIn from './pages/Sign-in'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div className=' w-full h-full'>
+     <SignIn/>
+    </div>
   )
 }
 
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,16 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 
 import Navigation from "./components/Navigation";
+import UserProvider from "./context/User-context";
+import SignIn from "./pages/Sign-in";
 
 export default function routes() {
   return (
     <>
       <BrowserRouter>
         <Navigation />
-
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
+        <UserProvider>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </UserProvider>
       </BrowserRouter>
     </>
   );
