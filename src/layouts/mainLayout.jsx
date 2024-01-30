ffc203a254fcad1478aa98a5e4a169ed596c2e74
Ibrahim Mansour <Ibrahim0Mansour@gmail.com>
2024-01-30 11:43:59 +0100

    testing again..

--- src/layouts/mainLayout.jsx
+++ src/layouts/mainLayout.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 import { Routes, Route, useLocation } from 'react-router-dom';
-
+import { useUserContext } from '../context/User-context';
 export default function MainLayout() {
   const location = useLocation();
   const isSignin = location.pathname === '/signin';
+  const { user } = useUserContext();
 console.log(location.pathname)
 console.log(isSignin)
   return (
     <>
-      {!isSignin && <Navigation />}
+      {!isSignin && user && <Navigation />}
       
       <Routes>
         <Route path="/*" />
