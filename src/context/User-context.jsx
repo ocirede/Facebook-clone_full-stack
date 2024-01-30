180c17161e80a1389ebe57a1e04a04b1d1d31a45
Ibrahim Mansour <Ibrahim0Mansour@gmail.com>
2024-01-30 14:19:09 +0100

    fixing quiries

--- src/context/User-context.jsx
+++ src/context/User-context.jsx
@@ -62,7 +62,7 @@ const UserProvider = ({ children }) => {
       localStorage.setItem("token", user.token);
       console.log("set token", user);
       e.target.reset();
-      setUser(user.user.firstname);
+      setUser(user.firstname);
       navigate("/home");
 
       console.log(user);
